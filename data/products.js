f19ff83b6ffa1415cdef7e305c4d27baea956007
RSXII <ryan@ryandschultz.com>
2017-12-11 20:21:13 -0700

    Changed products display to use an object, fixed selectable text on scroll menu.

---
+++ data/products.js
@@ -0,0 +1,10 @@
+let productsData = {
+    'mClothes': {
+        'item1': 'suit.png',
+        'item2': 'suit2.png'
+    },
+    'wClothes': {
+        'item1': 'dress.png',
+        'item2': 'dress2.png'
+    }
+};
--- scripts/functions.js
+++ scripts/functions.js
@@ -2,13 +2,15 @@ const sliderMenu = document.getElementById('sliderMenu');
 let sliderPosition = document.getElementById('sliderMenu').offsetLeft;
 let moveSlider;
 const theProducts = document.getElementById('theProducts');
-const theLoop = function(section){
-    for(let i = 1; i < 3; i++){
-        theProducts.innerHTML += "<div class='product col-sm-12 col-md-6 col-lg-4' style='background-image: url(images/" 
-        + section + i + ".png); background-position: center'></div>";
-    }
-}
 
+const theLoop = function(section){
+    let theList = productsData[section];
+    for(let item in theList){
+        theProducts.innerHTML += "<div class='col-sm-12 col-md-6 col-lg-4 itemContainer'><div class='product col-sm-12' style='background-image: url(images/" 
+                + theList[item] + "); background-position: center'> <div class='overlay'></div></div> </div>";
+    }
+    
+}
 
 sliderMenu.addEventListener("mousedown", function(e){
     e = e || window.event;
@@ -18,7 +20,6 @@ sliderMenu.addEventListener("mousedown", function(e){
     let mouseMoveCall = function(e){
         let newPosition = e.clientX;
         document.getElementById('sliderMenu').style.left = newPosition - x + y + "px";
-        console.log(newPosition);
     }
         sliderMenu.addEventListener("mousemove", mouseMoveCall);
     
@@ -28,17 +29,22 @@ sliderMenu.addEventListener("mousedown", function(e){
     }, true)
 }, false);
 
-document.onload = fillContent("m-c-");
+document.onload = fillContent('showAll');
 
 function fillContent(section){
+    let theSection = section;
     theProducts.innerHTML = "";
-    if(section != 'showall'){
-           theLoop(section);
+    let sectionLinks = document.getElementsByClassName('sectionLinks');
+    for(i = 0; i < sectionLinks.length; i++){
+        sectionLinks[i].style.color = "white";
+    }
+    document.getElementById(section).style.color = "gold";
+    if(section != 'showAll'){
+           theLoop(theSection);
     }else{
-        let allProd = ['m-c-', 'w-c-'];
-        allProd.forEach(function(section){
-           theLoop(section);
-        })
+     for(let list in productsData){
+         theLoop(list);
+     }
     }
 }
 
